27e66233eb4777c2013d3ca01c5c37201098fd38
Igor <32igor23@mail.ru>
2018-08-30 11:58:41 +0300

    test

--- js/main.js
+++ js/main.js
@@ -7,12 +7,34 @@ $(function (){
     $('<div class="clone-nav"><div>').insertBefore('nav').css('height', navHeight).hide();
 
     $(window).scroll(() => {
-        if ($(window).scrollTop() >= navPos) {
+        if ($(window).scrollTop() >= navPos && $(window).width() >= 768) {
             $('#nav').addClass('fixed shadow');
             $('.clone-nav').show();
         }  else {
             $('#nav').removeClass('fixed shadow');
             $('.clone-nav').hide();
+        }
+    });
+
+    //Burger menu
+    $('.nav-menu').on('click',function (){
+       $('#nav').css({
+           'transform': 'translateX(0)'
+       });
+       $('.overlay').toggleClass('overlay-act');
+    });
+    $('.close-btn').on('click',function (){
+        $('#nav').css({
+            'transform': 'translateX(-3000px)'
+        });
+        $('.overlay').toggleClass('overlay-act');
+    });
+    $(window).resize(function (){
+        if ($(window).width() >= 768 ) {
+            $('#nav').css('transform','translateX(0)');
+            $('.clone-nav').hide();
+        } else {
+            $('#nav').css('transform','translateX(-3000px)');
         }
     });
 });
